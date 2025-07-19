c23e678584356589b135a0e6943f4c3727e92ecb
Aryo Bismo Nugroho <aryobismo23@gmail.com>
2025-07-19 10:18:30 +0700

    feat: menambahkan koneksi db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI || ''),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    // 🔍 Debug log
+    console.log('MONGO_URI:', process.env.MONGO_URI);
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI belum didefinisikan di file .env!');
+    }
+  }
+}
